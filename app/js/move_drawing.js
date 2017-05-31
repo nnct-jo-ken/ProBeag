5eefa3df534729b5d098465e76722606fd861463
Stick-Figures <radwimps.04.13.b@gmail.com>
2017-05-31 20:39:08 +0900

    ちょっとした修正

--- app/js/move_drawing.js
+++ app/js/move_drawing.js
@@ -3,12 +3,14 @@ $.jCanvas.defaults.layer = true;
 
 var cicle = document.getElementById("cicle");
 var rect = document.getElementById("rect");
-var figuers = [];
-var load_figuers = 0;
+var Image = document.getElementById("for");
 var compile = document.getElementById("compile");
+var figuers = [];
+var for_figuers = [];
+var load_figuers = 0;
 var count_Rect = 1;
 var count_Ellipse = 1;
-var x_point = 200;
+var count_Image = 1;
 var obj_x;
 var obj_y;
 //配列に入れてtoString()で文字列に直している
@@ -62,12 +64,29 @@ function Compile(obj,Obj,count_obj){
   for(var i = 1;i < count_obj;i++){
     obj_x = $("#" + obj + "_x" + i).val();
     obj_y = $("#" + obj + "_y" + i).val();
-    if (typeof obj_x == "undefined" || obj_x == ""){
+
+    $(function(){
+      $("#" + obj + "_x" + i).change(function(){
+        obj_x = obj_x.replace(/[Ａ-Ｚａ-ｚ０-９－！”＃＄％＆’（）＝＜＞，．？＿［］｛｝＠＾～￥]/g, function(s) {
+            return String.fromCharCode(s.charCodeAt(0) - 65248);
+        });
+        $("#" + obj + "_x" + i).val(obj_x);
+      }).change();
+      $("#" + obj + "_y" + i).change(function(){
+        obj_y = obj_y.replace(/[Ａ-Ｚａ-ｚ０-９－！”＃＄％＆’（）＝＜＞，．？＿［］｛｝＠＾～￥]/g, function(s) {
+            return String.fromCharCode(s.charCodeAt(0) - 65248);
+        });
+        $("#" + obj + "_y" + i).val(obj_y);
+      }).change();
+    })
+
+    if (typeof obj_x == "undefined" || obj_x == "" ||isNaN($("#" + obj + "_x" + i).val())){
       obj_x = 0;
     }
-    if (typeof obj_y == "undefined" || obj_y == ""){
+    if (typeof obj_y == "undefined" || obj_y == "" ||isNaN($("#" + obj + "_y" + i).val())){
       obj_y = 0;
     }
+
     $("canvas").setLayer(Obj + i, {
       x: obj_x,
       y: obj_y
@@ -104,4 +123,23 @@ cicle.addEventListener("click",function(){
   }
   var ellipse_code = "ellipse(" + '<input type="text" size="4" id="ellipse_x">' + "," + '<input type="text" size="4" id="ellipse_y">' + ",w,h); " + "\n";
   literal(ellipse_code);
+},false);
+
+Image.addEventListener("click",function(){
+  ++count_Image;
+  for(var i = 1;i < count_Image;i++){
+    $("canvas").drawImage({
+      layer:true,
+      name:"Image" + i,
+      source:"img/black.png",
+      x: 100,
+      y: 100,
+      width : 100,
+      height : 100,
+      fromCenter : false,
+      draggable : true
+    });
+  }
+  var for_code = "for(int i =" + '<input type="text" size="4" id="for_init">' + "; i" + '<input type="text" size="4" id="for_ctr">' + "; i" + '<input type="text" size="4" id="for_incr">' + ";){" + "\n";
+  literal(for_code);
 },false);
