b5b312726facc82b9865efee3a1af77feaee4fd0
Hiroki.T <hirodora@me.com>
2017-06-22 22:33:18 +0900

    必要な関数などの配置 なぜか挙動がおかしいので修正していく。

--- app/js/main.js
+++ app/js/main.js
@@ -27,3 +27,18 @@ window.addEventListener("DOMContentLoaded", function(){
     pres[i].firstChild.data = pres[i].firstChild.data.replace(/^ +/gm,"");
   }
 },false);
+
+//tab用関数
+function openItem(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
